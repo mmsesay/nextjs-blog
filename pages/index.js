c95101cb76901d02333c18315a72b365e45af720
Muhammad Sesay <maej_01@hotmail.com>
2020-06-27 16:05:01 +0000

    refactor: changed layout name

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
+import Layout, { siteTitle } from '../components/Layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Date from '../components/date'
